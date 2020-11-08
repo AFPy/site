74d00312871f237b6065eb299e56068c2ffaea15
Jules Lasne <jules.lasne@gmail.com>
2020-11-08 16:55:00 +0100

    Added posts page

--- afpy/routes/home.py
+++ afpy/routes/home.py
@@ -9,6 +9,7 @@ from peewee import DoesNotExist
 
 from afpy.models.NewsEntry import NewsEntry
 from afpy.static import AFPY_ROOT
+from afpy.static import NEWS_PER_PAGE
 
 home_bp = Blueprint("home", __name__)
 
@@ -48,10 +49,24 @@ def render_rest(name):
     return render_template("pages/rst.html", body_id=name, html=parts["body"], title=parts["title"])
 
 
-@home_bp.route("/posts/<post_id>")
+@home_bp.route("/posts/<int:post_id>")
 def post_render(post_id: int):
     try:
         post = NewsEntry.get_by_id(post_id)
     except DoesNotExist:
         abort(404)
     return render_template("pages/post.html", body_id="post", post=post, name=post.title)
+
+
+@home_bp.route("/posts/page/<int:current_page>")
+def posts_page(current_page: int = 1):
+    total_pages = (NewsEntry.select().where(NewsEntry.state == "published").count() // NEWS_PER_PAGE) + 1
+    posts = NewsEntry.select().where(NewsEntry.state == "published").paginate(current_page, NEWS_PER_PAGE)
+    return render_template(
+        "pages/posts.html",
+        body_id="posts",
+        posts=posts,
+        title="Actualités",
+        current_page=current_page,
+        total_pages=total_pages,
+    )
--- afpy/static.py
+++ afpy/static.py
@@ -7,4 +7,6 @@ FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
 DB_NAME = os.getenv("DB_NAME")
 
+NEWS_PER_PAGE = 12
+
 AFPY_ROOT = os.path.join(os.path.dirname(__file__), "../")  # refers to application_top
