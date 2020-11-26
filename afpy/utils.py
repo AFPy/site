93bb1249a2f119f18433193266f4c7b17f2ca356
Jules Lasne <jules.lasne@gmail.com>
2020-11-26 17:21:17 +0100

    added linkify

--- afpy/utils.py
+++ afpy/utils.py
@@ -1,6 +1,10 @@
+from functools import partial
+
 import bleach
 import markdown2
 
 
 def markdown_to_html(content):
-    return bleach.sanitizer.Cleaner().clean(markdown2.markdown(content))
+    return bleach.sanitizer.Cleaner(
+        filters=[partial(bleach.linkifier.LinkifyFilter, skip_tags=["pre"], parse_email=False)]
+    ).clean(markdown2.markdown(content))
