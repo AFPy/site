31a590deb2b86831ea6707b8f6487c461774712e
Jules Lasne <jules.lasne@gmail.com>
2020-12-04 14:57:37 +0100

    More bleach config

--- afpy/utils.py
+++ afpy/utils.py
@@ -3,10 +3,82 @@ from functools import partial
 import bleach
 import markdown2
 
-ALLOWED_TAGS = ["a", "abbr", "acronym", "b", "blockquote", "code", "em", "i", "li", "ol", "strong", "ul", "p", "br"]
+
+ALLOWED_TAGS = [
+    # Bleach Defaults
+    "a",
+    "abbr",
+    "acronym",
+    "b",
+    "blockquote",
+    "code",
+    "em",
+    "i",
+    "li",
+    "ol",
+    "strong",
+    "ul",
+    # Custom Additions
+    "br",
+    "caption",
+    "cite",
+    "col",
+    "colgroup",
+    "dd",
+    "del",
+    "details",
+    "div",
+    "dl",
+    "dt",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "hr",
+    "img",
+    "p",
+    "pre",
+    "span",
+    "sub",
+    "summary",
+    "sup",
+    "table",
+    "tbody",
+    "td",
+    "th",
+    "thead",
+    "tr",
+    "tt",
+    "kbd",
+    "var",
+]
+
+ALLOWED_ATTRIBUTES = {
+    # Bleach Defaults
+    "a": ["href", "title"],
+    "abbr": ["title"],
+    "acronym": ["title"],
+    # Custom Additions
+    "*": ["id"],
+    "hr": ["class"],
+    "img": ["src", "width", "height", "alt", "align", "class"],
+    "span": ["class"],
+    "div": ["class"],
+    "th": ["align"],
+    "td": ["align"],
+    "code": ["class"],
+    "p": ["align", "class"],
+}
+
+ALLOWED_STYLES = []
 
 
 def markdown_to_html(content):
     return bleach.sanitizer.Cleaner(
-        tags=ALLOWED_TAGS, filters=[partial(bleach.linkifier.LinkifyFilter, skip_tags=["pre"], parse_email=False)]
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        styles=ALLOWED_STYLES,
+        filters=[partial(bleach.linkifier.LinkifyFilter, skip_tags=["pre"], parse_email=False)],
     ).clean(markdown2.markdown(content))
