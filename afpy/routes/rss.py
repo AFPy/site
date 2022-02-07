4c8a05fe12374121765b58ac2be619008503665a
Julien Palard <julien@palard.fr>
2022-02-07 15:38:42 +0100

    We migrated to makemake, see https://planet.afpy.org.

--- afpy/routes/rss.py
+++ afpy/routes/rss.py
@@ -36,29 +36,3 @@ def feed_rss(type):
         link=url_for("rss.feed_rss", type=type, _external=True),
         type=type,
     )
-
-
-@rss_bp.route("/planet/")
-@rss_bp.route("/planet/rss.xml")
-def planet_rss():
-    entries = []
-    with open(f"{config.AFPY_ROOT}/afpy/data/data.json", "r") as handle:
-        planet_items = json.load(handle)["planet"]
-    for name, url in planet_items.items():
-        for entry in feedparser.parse(url).entries:
-            if hasattr(entry, "updated_parsed"):
-                date = entry.updated_parsed
-            elif hasattr(entry, "published_parsed"):
-                date = entry.published_parsed
-            else:
-                date = time.time()
-            entry["timestamp"] = time.mktime(date) if date else time.time()
-            entries.append({"feed": name, "content": entry})
-    entries.sort(reverse=True, key=lambda entry: entry["content"]["timestamp"])
-    return render_template(
-        "pages/planet_rss.xml",
-        entries=entries,
-        title="Planet Python francophone",
-        description="Nouvelles autour de Python en français",
-        link=url_for("rss.planet_rss", _external=True),
-    )
